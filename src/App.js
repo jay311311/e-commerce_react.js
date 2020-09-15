a54964bd20000ab7cfd2daacc4f365cc37341fc1
jay311311 <jay311311@gmail.com>
2020-09-15 18:37:35 +0900

    making components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./compoenets/Navbar"
+import ProductList from "./compoenets/ProductList"
+import Details from "./compoenets/Details"
+import Cart from "./compoenets/Cart"
+import Default from "./compoenets/Default"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar/>
+      <ProductList/>
+      <Details/>
+      <Cart/>
+      <Default/>
+    </React.Fragment>
+
   );
 }
 
---
+++ src/compoenets/Navbar.js
@@ -0,0 +1,11 @@
+import React, {Component}from "react"
+
+export default class Navbar extends Component{
+    render(){
+        return(
+            <div>
+                <h3>hello fom nav bar</h3>
+            </div>
+        )
+    }
+}
