8629c8b8da51a283959aa80c5f14fc4ffacbeb9d
jay311311 <jay311311@gmail.com>
2020-09-16 23:38:49 +0900

    changing data by this.setState

--- src/context.js
+++ src/context.js
@@ -9,7 +9,8 @@ const ProductContext = React.createContext();
 class ProductProvider extends Component {
    state={
        products:[],
-       detailProduct:detailProduct
+       detailProduct:detailProduct,
+       cart:[]
    }
 
    componentDidMount(){
@@ -28,13 +29,32 @@ class ProductProvider extends Component {
        })
    }
 
-   handleDetail = () =>{
-       console.log("hello from detail")
+   getItem = id  =>{
+       const product =this.state.products.find(item =>item.id === id)
+       return  product;
+   }
+
+   handleDetail = id =>{
+       const product = this.getItem(id);
+       this.setState(()=>{
+           return {detailProduct : product}
+       })
    }
 
    
-   addToCart = (id) =>{
-    console.log(`hello from add to Cart id is${id}`)
+   addToCart = id =>{
+   let tempProducts = [...this.state.products];
+   const index = tempProducts.indexOf(this.getItem(id));
+   const product = tempProducts[index];
+   product.inCart = true;
+   product.count = 1
+   const price = product.price;
+   product.total = price;
+
+this.setState(()=>{
+    return {product : tempProducts, cart:[...this.state.cart,product]}
+},()=>{console.log(this.state)}
+)
 }
 
 
