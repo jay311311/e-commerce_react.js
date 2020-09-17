75a83685f42f387ce104601f9a7d76ed5ed05e18
jay311311 <jay311311@gmail.com>
2020-09-18 00:15:46 +0900

    making a Quantitiy  buttons function in CarList Component

--- src/context.js
+++ src/context.js
@@ -75,12 +75,50 @@ class ProductProvider extends Component {
     }
 
     increment = (id) => {
-        console.log("this is increment method")
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(()=>{
+            return{ 
+                cart:[...tempCart]
+                }
+        },()=>{
+            this.addTotals();
+        })
     }
     decrement = (id) => {
-        console.log("this is decrement method")
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id);
+
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count - 1;
+
+        if(product.count === 0){
+            this.removeItem(id);
+        }else{
+            product.total = product.count * product.price;
+        }
+        
+
+        this.setState(()=>{
+            return{ 
+                cart:[...tempCart]
+                }
+        },()=>{
+            this.addTotals();
+        })
+
     }
-    removeItem= (id)=>{
+
+    removeItem= id => {
         let tempProducts = [...this.state.products]
         let tempCart = [...this.state.cart]
 
@@ -101,6 +139,7 @@ class ProductProvider extends Component {
             this.addTotals()
         })
     }
+
     clearCart = () =>{
         this.setState(()=>{
             return{cart:[]};
